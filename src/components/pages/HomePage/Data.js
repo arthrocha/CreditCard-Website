c3154107a44af72c9638f6239cbe1ddd911b68bf
Arthur de Andrade <arthurderochaa@gmail.com>
2021-04-25 23:30:09 -0300

    commit padrão

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,54 @@
+export const homeObjOne = {
+    lightBg:false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Acesso Exclusivo',
+    headLine: "Aposentados e Servidores Públicos",
+    description: "Garantimos a melhor negociação com contratação online e sem burocracia! Crédito disponível até para quem está com pouca ou nenhuma margem!",
+    buttonLabel: "Simule Agora!",
+    imgStart: "",
+    img:"images/svg-1.svg",
+    alt:"Credit Card"
+}
+
+export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '100% SEGURO',
+    headLine: 'Todo o processo é protegido, os seus dados estão seguros!',
+    description:
+      'A Caixa do Aposentado se preocupa com os seus dados pessoais, por isso garantimos a total segurança!',
+    buttonLabel: 'Saiba Mais',
+    imgStart: '',
+    img: 'images/svg-5.svg',
+    alt: 'Vault'
+  };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'FÁCIL E RÁPIDO',
+    headLine: 'Todo o processo de simulação é fácil e rápido',
+    description:
+      "Receba o resultado da sua simulação em menos de 5 minutos. Você só precisa responder algumas perguntas, adicionar algumas informações, e o resto a gente faz!",
+    buttonLabel: 'Simular Agora!',
+    imgStart: 'start',
+    img: 'images/svg-7.svg',
+    alt: 'Vault'
+  };
+  
+  export const homeObjFour = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'DATA ANALYTICS',
+    headLine: 'Every transaction is stored on our secure cloud database',
+    description:
+      'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: 'images/svg-8.svg',
+    alt: 'Vault'
+  };
